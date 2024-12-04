1662abe3bdc0f75c0473e578ad301c4872c161cd
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-04 11:02:35 +0600

    initial commit

--- first_project/__manifest__.py
+++ first_project/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'My First Module',
     'category': 'Uncategory',
     'summary': 'Create My First Module',
+    'sequence':'-100',
     'version': '0.1.2.2',
     'depends': [],
     'data': [
@@ -9,10 +10,7 @@
         'views/view_school_menu.xml',
         'views/view_student_menu.xml',
     ],
-    'demo': [
-        'demo/demo.xml',
-    ],
     'installable': True,
     'application': True,
-    'auto_install': False
+    'auto_install': False,
 }
--- first_project/models/student.py
+++ first_project/models/student.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 class SchoolStudent(models.Model):
     _name = 'school.student'
@@ -11,5 +12,15 @@ class SchoolStudent(models.Model):
     ], string='Gender')
     date_of_birth = fields.Date(string='Date of Birth')
     age = fields.Integer(string='Age', compute='_compute_age')
-    guardian = fields.Char(string='Guardian')
+    guardian = fields.Char(string='Guardian', default='Odoo Mates')
     note = fields.Text(string='Description')
+
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
